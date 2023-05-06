8caeb27b11a5a4956beacf09bfaf411c0bad0fb7
Kai-Surface <129895125+Kai-Surface@users.noreply.github.com>
2023-05-06 15:29:09 -0700

    Add project files.

---
+++ Practice_Aggregation_and_Containment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice_Aggregation_and_Containment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instructor will = new Instructor("Will", "Cram");
+            SchoolProgram prog124 = new SchoolProgram("Programming", "124", "J102", will);
+            prog124.AddStudent("Ana", "Mendes", 100, 100);
+            Console.WriteLine(will);
+            //Console.WriteLine(Instructor);
+            Console.WriteLine(prog124);
+        }
+    }
+}
---
+++ Practice_Aggregation_and_Containment/SchoolProgram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Aggregation_and_Containment
+{
+    internal class SchoolProgram
+    {
+        //Fields
+        string _className;
+        string _classNumber;
+        string _roomNumber;
+        Instructor _instructor;
+        List<Student> _classRoster;
+        int _numOfStudents;
+
+        //Constructor
+        public SchoolProgram(string className, string classNumber, string roomNumber, Instructor instructor)
+        {
+            ClassName = className;
+            ClassNumber = classNumber;
+            RoomNumber = roomNumber;
+            Instructor = instructor;
+        }
+
+        //Property
+        public string ClassName { get => _className; set => _className = value; }
+        public string ClassNumber { get => _classNumber; set => _classNumber = value; }
+        public string RoomNumber { get => _roomNumber; set => _roomNumber = value; }
+        public int NumOfStudents { get => _numOfStudents; set => _numOfStudents = value; }
+        internal Instructor Instructor { get => _instructor; set => _instructor = value; }
+        internal List<Student> ClassRoster { get => _classRoster;  }
+
+        //Methods
+        public void AddStudent(Student student)
+        {
+            _classRoster.Add(student);
+        }
+
+        public void AddStudent(string firstName, string lastName, int csiGrade, int genEdGrade)
+        {
+
+            Student student = new(firstName, lastName, csiGrade, genEdGrade);
+            _classRoster.Add(student);
+        }
+
+        public override string ToString()
+        {
+            return $"{_className} : {_classNumber} - {_instructor} - Number of Students: {_numOfStudents}";
+        }
+    }
+}
